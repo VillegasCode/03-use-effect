0b75c365902d7e3da1808c4e86e9511079e2c63b
Carlos Villegas <carlos.villegasch@gmail.com>
2023-05-25 20:42:31 -0500

    Mount and Unmount components with hook useEffect

--- src/components/AvisoComponent.js
+++ src/components/AvisoComponent.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export const AvisoComponent = () => {
+
+useEffect(() => {
+    //Cuando el componente se monta
+    alert("El componente AvisoComponent está montado");
+
+    //Cuando el componente se desmonta
+    return () => {
+        alert("AvisoComponent DESMONTADO");
+    };
+}, []); //Se ejecuta una vez porque le paso el array vacío
+
+
   return (
     <div>
         <hr />
