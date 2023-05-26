dfd0af6a583fa1f534f26d3b91f04e3548881c08
Carlos Villegas <carlos.villegasch@gmail.com>
2023-05-25 20:20:41 -0500

    If counter is major to 20 then we load <AvisoComponent>

--- src/components/PruebasComponent.js
+++ src/components/PruebasComponent.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react'
+import { AvisoComponent } from './AvisoComponent';
 
 export const PruebasComponent = () => {
   
@@ -46,6 +47,8 @@ export const PruebasComponent = () => {
             <button onClick={cambiarFecha}>Cambiar Fecha</button>
         </p>
 
+        //Condicional Ternaria, si se cumple la condición se carga el componente
+        {contador > 20 && <AvisoComponent />}
     </div>
   )
 }
