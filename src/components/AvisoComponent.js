861293b29a34ef97d831de815bafb391f97ab285
Carlos Villegas <carlos.villegasch@gmail.com>
2023-05-25 20:21:32 -0500

    Show Alert

---
+++ src/components/AvisoComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const AvisoComponent = () => {
+  return (
+    <div>
+        <hr />
+        <h3>Hemos superado los 20 cambios</h3>
+        <button>Mostrar Alerta</button>
+    </div>
+  )
+}
